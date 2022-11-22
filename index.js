0b53878d45a585d71b2914863541f26113325796
mofazzal-titon <mofazzalcse39@gmail.com>
2022-11-23 03:08:50 +0600

    commited

---
+++ index.js
@@ -0,0 +1,39 @@
+ x = 0 ;
+var click = 0;
+var count = 0;
+
+function createMessage(){
+    x = x+2;
+    document.getElementById("id1").innerHTML= x ;
+    document.getElementById("id1").style.color = 'pink'  ;
+
+}
+
+function decrease(){
+
+    click++ 
+    var random = Math.random()
+    var ran = Math.floor(random*10);
+    
+    
+
+    if (ran == 5 || ran == 7 ) {
+
+        count++
+        var per = (count/click)*100;
+        document.getElementById('id1').style.background = "red"
+        document.getElementById('id2').innerHTML = "Hitted  " + count + 
+        " times" + ' from ' + click + " clicks and its, " + per + " %" ;
+
+
+    } else {
+        document.getElementById('id1').style.background = "gray"
+    }
+    
+
+    document.getElementById("id1").innerHTML= ran ;
+    document.getElementById("id1").style.color = 'cyan'  ;
+
+
+}
+
